02a10938a11ac076388a5ba01eed4dba6ae0636d
kirill.morozov_1994@mail.ru <kirill.morozov_1994@mail.ru>
2018-12-01 22:47:54 +0300

    Урок 9 доп задание

--- lesson_10_additional/script.js
+++ lesson_10_additional/script.js
@@ -5,8 +5,10 @@ window.addEventListener('DOMContentLoaded', function () {
 		input.addEventListener('input', maskInput);
 		input.addEventListener('click', maskInput);
 
-	function maskInput() {
-		let matrix = this.defaultValue,
+	function maskInput(event) {
+		//if(this.length != 15)
+		//console.log(i);
+		let matrix = this.defaultValue;
 			i = 0,
 			def = matrix.replace(/\D/g, ""),
 			val = this.value.replace(/\D/g, "");
@@ -14,38 +16,38 @@ window.addEventListener('DOMContentLoaded', function () {
 			val = def;
 		}
 		matrix = matrix.replace(/[_\d]/g, function (a) {
-			console.log(val.charAt(i));
 			if (val.charAt(i) == "") {
 				return "_";
 			}
 			return val.charAt(i++);
 		});
-		console.log(matrix);
 		this.value = matrix;
-		i = matrix.lastIndexOf(val.substr(-1));
+		if (i == 15) {
+			console.log(i);
+			return false;
+		} else {
+			i = matrix.lastIndexOf(val.substr(-1));
+		}
 		if (i < matrix.length && matrix != this.defaultValue) {
-			i++;
+				i++;
 		} else {
 			i = matrix.indexOf("_");
 		}
-		console.log(i);
-		console.log(this);
 		setCursorPosition(i, this);
 	}	
 
 	function setCursorPosition(pos, elem) {
-		elem.focus();
-		console.log(elem);
-		console.log(elem.setSelectionRange);
+		if(pos == 15) {
+			return false;
+		}
 		if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
 		else if (elem.createTextRange) {
 			let range = elem.createTextRange();
-			console.log(range);
 			range.collapse(true);
 			range.moveEnd("character", pos);
 			range.moveStart("character", pos);
 			range.select();
-			console.log(range);
+			//console.log(range);
 		}
 	}
 
--- lesson_9_additional/js/script.js
+++ lesson_9_additional/js/script.js
@@ -1,140 +1,74 @@
 window.addEventListener('DOMContentLoaded', function () {
 	
 	'use strict';
-	//Информационные табы ==============================================
-	let tab = document.querySelectorAll('.info-header-tab'),
-			info = document.querySelector('.info-header'),
-			tabContent = document.querySelectorAll('.info-tabcontent');
-	
-	function hideTabContent(a) {
-		for(let i = a; i < tabContent.length; i++) {
-			tabContent[i].classList.remove('show');
-			tabContent[i].classList.add('hide');
-		}
-	}
-
-	hideTabContent(1);
-
-	function showTabContent(b) {
-		if(tabContent[b].classList.contains('hide')) {
-			tabContent[b].classList.remove('hide');
-			tabContent[b].classList.add('show');
-		}
-	}
-
-	info.addEventListener('click', function (event) {
-		let target = event.target;
-		if(target && target.classList.contains('info-header-tab')) {
-			for(let i = 0; i < tab.length; i++) {
-				if(target == tab[i]) {
-					hideTabContent(0);
-					showTabContent(i);
-					break;
-				}
-			}
-		}
-	});
-	//Информационные табы ==============================================
-
-	//Таймер ===========================================================
-	let deadLine = '2018-12-01';
-
-	function getTimerRemaining(endtime) {
-		let t = Date.parse(endtime) - Date.parse(new Date()),
-				seconds = Math.floor((t/1000) % 60),
-				minutes = Math.floor((t/1000/60) % 60),
-				hours = Math.floor((t/(1000*60*60)));
-
-				return {
-					'total' : t,
-					'hours' : hours,
-					'minutes' : minutes,
-					'seconds' : seconds
-				};
-	}
-	
-	function setClock(id, endtime) {
-		let timer = document.getElementById(id),
-				hours = timer.querySelector('.hours'),
-				minutes = timer.querySelector('.minutes'),
-				seconds = timer.querySelector('.seconds');
-				let timeInterval = setInterval(upDateClock, 1000);
-
-				function upDateClock() {
-					let t = getTimerRemaining(endtime);
-						if (t.hours < 10) {
-							t.hours = '0' + t.hours;
-						}
-						if (t.minutes < 10) {
-							t.minutes= '0' + t.minutes;
-						}
-						if (t.seconds < 10) {
-							t.seconds = '0' + t.seconds;
-						}
-							hours.textContent = t.hours;
-							minutes.textContent = t.minutes;
-							seconds.textContent = t.seconds;
-
-							if(t.total <= 0) {
-								clearInterval(timeInterval);
-								hours.textContent = '00';
-								minutes.textContent = '00';
-								seconds.textContent = '00';
-							}
-				}
-	}
-
-	setClock('timer', deadLine);
-	//Таймер ===========================================================
 
 	//Модальное окно ===================================================
-	let more = document.querySelector('.more'),
-			overlay = document.querySelector('.overlay'),
-			close = document.querySelector('.popup-close');
+		console.log(/Trident/i.test(navigator.userAgent));
+	let btn = document.querySelector('.btn'),
+			popup = document.querySelector('.popup'),
+			close = document.querySelector('.close');
 	
-	more.addEventListener('click', function () {
-		showPopup();
+	btn.addEventListener('click', function () {
+			showPopup();
 	});		
 	close.addEventListener('click', function () {
-		overlay.style.display = 'none';
-		more.classList.remove('more-splash');
-		document.body.style.overflow = '';
-	});
+		if (/Edge/i.test(navigator.userAgent)) {
+			popup.classList.remove('fade');
+			popup.classList.add('hide');
+		} else if (/Trident/i.test(navigator.userAgent)) {
+			popup.classList.remove('fade');
+			popup.classList.add('hide');
+		} else if(/Mobi/i.test(navigator.userAgent)) {
+			popup.style.opacity = "0";
+			popup.style.transform = "translateY(-200%)";
+		} else {
+			let trans = 0,
+				op = 1;
+			let id_1 = setInterval(hide, 1);
 
-	let infoBlock = document.querySelector('.info');
-
-	infoBlock.addEventListener('click', function (event) {
-			if (event.target && event.target.classList.contains('description-btn')) {
-				showPopup();
-			}
-	});
-
-	function showPopup() {
-		if (document.documentMode || /Edge/.test(navigator.userAgent)) {
-			overlay.style.display = 'block';
-			event.target.classList.add('more-splash');
-			document.body.style.overflow = 'hidden';
-		}
-		if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-			overlay.classList.remove('fade');
-			overlay.style.display = 'block';
-			document.body.style.overflow = 'hidden';
-		}
-			let trans = 200,
-					op = 0;
-		overlay.classList.remove('fade');			
-		let id = setInterval(frame, 1);
-				function frame() {
-					if (trans == 0 || op == 1) {
-						clearInterval(id);
-					} else {
-						overlay.style.display = 'block';
-						trans = trans - 0.5;
-						op = op + 0.0025;
-						overlay.style.transform = `translateY(${trans}%)`;
-						overlay.style.opacity = `${op}`;
-					}
+			function hide() {
+				if (trans == 200 || op == 0) {
+					clearInterval(id_1);
+				} else {
+					trans = trans + 0.5;
+					op = op - 0.0025;
+					popup.style.transform = 'translateY(' + trans + '%)';
+					popup.style.opacity = op;
 				}
+			}
+		}
+	});
+
+	//console.log(/Edge/i.test(navigator.userAgent));
+	function showPopup() {
+
+		if (/Edge/i.test(navigator.userAgent)) {
+			popup.classList.remove('hide');
+			popup.classList.add('fade');
+		} else if (/Trident/i.test(navigator.userAgent)) {
+			popup.classList.remove('hide');
+			popup.classList.add('fade');
+			close.style.top = '-20%';
+		} else if (/Mobi/i.test(navigator.userAgent)) {
+			popup.style.opacity = "1";
+			popup.style.transform = "translateY(0%)";
+		} else {
+			let trans = 200,
+				op = 0;
+			let id = setInterval(fade, 1);
+
+			function fade() {
+				if (trans == 0 || op == 1) {
+					clearInterval(id);
+				} else {
+					trans = trans - 0.5;
+					op = op + 0.0025;
+					popup.style.transform = 'translateY(' + trans + '%)';
+					popup.style.opacity = op;
+				}
+			}
+		}
+
 	}
 	//Модальное окно ==================================================
 });
