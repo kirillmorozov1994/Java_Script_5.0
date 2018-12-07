8e1aef4e11664e9fae1a7f9ec72321a0509574df
kirill.morozov_1994@mail.ru <kirill.morozov_1994@mail.ru>
2018-12-07 19:21:21 +0300

    Урок 14 испр

--- lesson_14_additional/js/script.js
+++ lesson_14_additional/js/script.js
@@ -3,21 +3,29 @@ $(document).ready(function () {
 //Модальное окно ====================================================================
 	$('.main_btna, .main_btn, .main_nav li:eq(1)').on('click', function () {
 		$('.overlay').fadeIn('slow');
-		$('.modal').css({'display': 'block', 'marginTop': '-30%', 'width': '60%'}).animate(
+		$('.modal').css({'width': '60%'}).slideDown('slow').animate(
 			{
 				opacity: '1',
-				marginTop: '10%',
 				width: '40%'
-			}, 'slow');
+			},
+		 	{
+				queue: false,
+				duration: 'slow'
+			},
+			'slow');
 	});
 	$('.overlay, .close').on('click', function () {
 		$('.overlay').fadeOut('slow');
-		$('.modal').animate(
+		$('.modal').slideUp('slow').animate(
 			{
 				opacity: '0',
-				marginTop: '-30%',
 				width: '60%'
-			}, 'slow');
+			},
+			{
+				queue: false,
+				duration: 'slow'
+			},
+			 'slow');
 	});
 //Модальное окно ====================================================================
 
--- lesson_14_mandatory/js/script.js
+++ lesson_14_mandatory/js/script.js
@@ -1,16 +1,10 @@
 $(document).ready(function () {
 	$('.main_btna, .main_btn, .main_nav li:eq(1)').on('click', function () {
 		$('.overlay').fadeIn('slow');
-		$('.modal').css({'display': 'block', 'marginTop': '-30%'}).animate(
-			{
-				marginTop: '10%'
-			}, 'slow');
+		$('.modal').slideDown('slow');
 	});
 	$('.overlay, .close').on('click', function () {
 		$('.overlay').fadeOut('slow');
-		$('.modal').animate(
-			{
-				marginTop: '-30%'
-			}, 'slow');
+		$('.modal').slideUp('slow');
 	});
 });
